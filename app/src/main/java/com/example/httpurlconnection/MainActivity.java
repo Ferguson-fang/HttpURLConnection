00507a24d087f57fc7ab0016906aff24a95d2bb5
Ferguson <834929497@qq.com>
2021-05-29 15:10:27 +0800

    up data level 2.

--- app/src/main/java/com/example/httpurlconnection/MainActivity.java
+++ app/src/main/java/com/example/httpurlconnection/MainActivity.java
@@ -12,35 +12,54 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.net.Proxy;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button get;
     private Button post;
+    private Button gson;
+    private Button gsonf;
     private TextView tv;
     private String url;
+    private Map<String,String> map = new HashMap<>();
     private MHander mHander = new MHander();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        Gson gson;
     }
 
     void init(){
         get = findViewById(R.id.btn_GET);
         post = findViewById(R.id.btn_POST);
         tv = findViewById(R.id.response);
+        gson = findViewById(R.id.btn_GSONTOJSON);
+        gsonf = findViewById(R.id.btn_GSONFROMJSON);
 
         get.setOnClickListener(this);
         post.setOnClickListener(this);
+        gson.setOnClickListener(this);
+        gsonf.setOnClickListener(this);
+
+        map.put("name","Ferguson");
+        map.put("age","19");
+        map.put("hobby","programming");
 
         url = "http://guolin.tech/api/china";
 
@@ -57,6 +76,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_POST:
 
                 break;
+
+            case R.id.btn_GSONTOJSON:
+                showGsonToJson(map);
+                break;
+
+            case R.id.btn_GSONFROMJSON:
+                String str =
+                  "{\"name\":\"Ferguson\",\"age\":\"19\",\"hobby\":\"programming\"}";
+                //{"name":"Ferguson","age":"23","hobby":"programming"}
+                showGsonFromJson(str);
+                break;
+        }
+    }
+    private void showGsonToJson(Map<String,String> jsonData){
+        Class<?> c = null;
+        try {
+            c = Class.forName("com.google.gson.Gson");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        try {
+            Method met = c.getMethod("toJson",Object.class);
+            String rv = null;
+            rv = (String)met.invoke(c.newInstance(),jsonData);
+            Log.e("sssa",rv);
+            tv.setText(rv);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void showGsonFromJson(String jsonData){
+        Class<?> c = null;
+        try {
+            c = Class.forName("com.google.gson.Gson");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        try {
+            Method met = c.getMethod("fromJson",String.class, Type.class);
+            PersonBean person = (PersonBean) met.invoke(c.newInstance(),jsonData,PersonBean.class);
+            String rv = "姓名:"+person.getName() + "年龄:" + person.getAge() + "爱好:" + person.getHobby();
+            Log.e("sss",rv);
+            tv.setText(rv);
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 
---
+++ app/src/main/java/com/example/httpurlconnection/PersonBean.java
@@ -0,0 +1,31 @@
+package com.example.httpurlconnection;
+
+public class PersonBean {
+    private String name;
+    private String age;
+    private String hobby;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+}
